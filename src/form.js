e9c180718d6f02ecdb4577929bb3e750e2b20712
Sam D. Lee <cto@idavollen.eu>
2016-08-17 22:48:57 +0200

    add support of defaultValue and new method formData

--- src/form.js
+++ src/form.js
@@ -11,6 +11,7 @@ export default class FormProvider extends Component {
     this.validators = createValidators(props.validators);
     this.fields = Object.keys(props.validators);
     this.state = this._initState();
+    this._initCallbacks();
   }
 
   _initState() {
@@ -25,11 +26,24 @@ export default class FormProvider extends Component {
   }
 
   componentWillMount() {
+    this.isInited = false
+  }
+
+  componentDidMount() {
+    this.isInited = true
+  }
+
+  _initCallbacks() {
     var cbs = {}
     const cb = field => (value, validate = true) => {
       let msg = validate && this.validators.validateField(field, value) || undefined;
       let fieldState = {};
       fieldState[field] = { value: value, msg: msg }
+      if (!this.isInited) {
+        this.setState(function(previousState, currentProps) {
+          return {...previousState, ...fieldState};
+        })
+      } else
       this.setState({ ...this.state, ...fieldState })
     }
     // add new validators on top of existing ones
@@ -44,24 +58,20 @@ export default class FormProvider extends Component {
         addValidator: addValidator(field)
       }
     });
-    cbs.validateForm = (formdata = {} ) => { 
-      if (!formdata || Object.keys(formdata).length === 0) {
-        formdata = {};
-        this.fields.forEach( field => {
-          formdata[field] = this.state[field] ? this.state[field].value : '';
-        })
-      }   
-      let fieldState = {}, err;
-      for (let field in formdata) {
-        let msg = this.validators.validateField(field, formdata[field]);
-        err = err || msg;
-        fieldState[field] = { value: formdata[field], msg }
-      }
+    cbs.validateForm = (...fields) => {
+      let formdata = {}, fieldState = {}, err=[];
+      fields = !fields || fields.length === 0? this.fields : fields;
+      fields.forEach( field => {
+        let fieldVal = this.state[field] ? this.state[field].value : '';
+        let msg = this.validators.validateField(field, fieldVal);
+        msg && err.push(msg)
+        fieldState[field] = { value: fieldVal, msg }
+      })
       this.setState({ ...this.state, ...fieldState });
-      return err
+      return err.length > 0? err : null
     }
     // add validators for a new form field, that is, there exists no validators for this field
-    cbs.addNewField = (field, ...validators) => {
+    cbs.addNewField = (field, defValue, ...validators) => {
       if (!this.fields.includes(field)) {
         this.fields.push(field);
         addValidator(field)(...validators);
@@ -72,16 +82,25 @@ export default class FormProvider extends Component {
         }  
         let fieldState = {};
         fieldState[field] = {
-          value: '',
+          value: defValue || '',
           msg: undefined
         }
         this.setState({ ...this.state, ...fieldState })
       }
     }
+    cbs.formData = (...fields) => {
+      let formdata = {};
+      fields = !fields || fields.length === 0? this.fields : fields;
+      fields.forEach( field => {
+        formdata[field] = this.state[field] ? this.state[field].value : '';
+      })
+      return formdata;
+    }
+    
     this.cbs = cbs;
   }
 
-  mapStateToProps() {
+  getProps() {
     var props = {}
     this.fields.forEach( field => {
       props[field] = Object.assign({}, this.cbs[field], {
@@ -90,18 +109,14 @@ export default class FormProvider extends Component {
         msg: this.state[field].msg
       })
     })
-    props.validateForm = this.cbs.validateForm;
-    props.addNewField = this.cbs.addNewField;
-    return props;
+    return Object.assign({},  this.cbs, props)
   }
 
   render() {
-    var props = this.mapStateToProps();
-    return (
-      <div>
-        { React.cloneElement(this.props.children, { ...props, validators: this.validators }) }
-      </div>
-    );
+    var props = this.getProps();
+    return <div className="react-form-ex">
+      { React.cloneElement(this.props.children, { ...props, validators: this.validators }) }
+    </div>
   }
 }
 
